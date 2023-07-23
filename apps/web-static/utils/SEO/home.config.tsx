2ed0c5b5a6fac562a4b8e51524f868693169532f
SRB <saurabhdsachan@gmail.com>
2023-07-23 17:39:18 +0530

    FInal

--- apps/web-static/utils/SEO/home.config.tsx
+++ apps/web-static/utils/SEO/home.config.tsx
@@ -4,7 +4,7 @@ import commonSEO from './default.config';
 
 const HomePageBaseSEO = {
   canonical: company.url,
-  title: 'Cash Advance | Get Instant Cash with Gogo',
+  title: 'Hyper-local Simplified | Gogo',
   description: company.description,
 };
 
@@ -12,16 +12,16 @@ const openGraph = {
   type: 'website',
   locale: 'en_US',
   url: company.url,
-  title: 'Cash Advance | Get Instant Cash with Gogo',
+  title: 'Hyper-local Simplified | Gogo',
   description:
-    'Gogo helps working-class Americans get $20 - $1,000 instant cash without interest or credit checks. Get instant cash now and pay later. Plans start at $2.47/month.',
+    'The ultimate app that empowers offline stores to seamlessly transition into the online realm while unlocking the power of data-driven analytics',
 };
 
 const twitter = {
   ...commonSEO.twitterOptions,
-  title: 'Cash Advance | Get Instant Cash with Gogo',
+  title: 'Hyper-local Simplified | Gogo',
   description:
-    'Gogo helps working-class Americans get $20 - $1,000 instant cash without interest or credit checks. Get instant cash now and pay later. Plans start at $2.47/month.',
+    'the ultimate app that empowers offline stores to seamlessly transition into the online realm while unlocking the power of data-driven analytics',
 };
 
 const additionalLinkTags = [
